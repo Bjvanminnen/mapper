258d1c0c4471050af3a4ee986bdd643501217760
Brent Van Minnen <bjvanminnen@gmail.com>
2016-12-19 10:45:58 -0800

    geolocation example

--- index.ios.js
+++ index.ios.js
@@ -15,39 +15,61 @@ import {
 export default class mapper extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+      <GeolocationExample />
+    );
+  }
+}
+
+class GeolocationExample extends React.Component {
+  state = {
+    initialPosition: 'unknown',
+    lastPosition: 'unknown',
+    updates: 0
+  };
+
+  watchID: null;
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        var initialPosition = JSON.stringify(position);
+        this.setState({initialPosition});
+      },
+      (error) => alert(JSON.stringify(error)),
+      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+    );
+    this.watchID = navigator.geolocation.watchPosition((position) => {
+      var lastPosition = JSON.stringify(position);
+      this.setState({lastPosition, updates: this.state.updates + 1});
+    });
+  }
+
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchID);
+  }
+
+  render() {
+    return (
+      <View>
+        <Text style={styles.title}>Initial position: </Text>
+        <Text style={styles.text}>{this.state.initialPosition}</Text>
+        <Text style={styles.title}>Current position: </Text>
+        <Text style={styles.text}>{this.state.lastPosition}</Text>
+        <Text style={styles.title}>{this.state.updates}</Text>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+  title: {
+    fontWeight: '500',
+    fontSize: 30,
+    marginTop: 30
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  text: {
+    fontSize: 20
+  }
 });
 
 AppRegistry.registerComponent('mapper', () => mapper);
