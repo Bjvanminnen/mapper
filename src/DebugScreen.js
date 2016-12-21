47baf3c3f5bbf01dc28569accd32297ef39b3077
Brent Van Minnen <bjvanminnen@gmail.com>
2016-12-20 17:05:38 -0800

    beginning of debug config

--- src/DebugScreen.js
+++ src/DebugScreen.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Picker, TextInput, Keyboard, TouchableWithoutFeedback } from 'react-native';
+import LabelledInputRow from './LabelledInputRow';
+import { RNLocation as Location } from 'NativeModules';
 
 export default class DebugScreen extends Component {
+  state = {
+    selectedItem: 0
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.onDistanceChange = this.onDistanceChange.bind(this);
+  }
+
+  onDistanceChange(val) {
+    const numVal = parseInt(val, 10);
+    Location.setDistanceFilter(numVal);
+  }
+
   render() {
     return (
-      <View>
-        <Text>This is the DebugScreen</Text>
+      <View style={{flex: 1}}>
+        <Text>
+          This is the DebugScreen
+        </Text>
+        <LabelledInputRow
+          label="distance"
+          onChange={this.onDistanceChange}
+        />
       </View>
     );
   }
---
+++ src/LabelledInputRow.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { View, Text, Picker, TextInput, Keyboard, TouchableWithoutFeedback, ScrollView } from 'react-native';
+
+const styles = {
+  input: {
+    height: 26,
+    borderWidth: 0.5,
+    borderColor: '#0f0f0f',
+    flex: 1,
+    fontSize: 13,
+    padding: 4,
+  },
+  label: {
+    width: 115,
+    alignItems: 'flex-end',
+    marginRight: 10,
+    paddingTop: 2
+  },
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    marginVertical: 2,
+  }
+};
+
+export default class LabelledNumberInput extends React.Component {
+  static propTypes = {
+    label: React.PropTypes.string.isRequired,
+    onChange: React.PropTypes.func
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.onEndEditing = this.onEndEditing.bind(this);
+  }
+
+  onEndEditing(event) {
+    if (this.props.onChange) {
+      this.props.onChange(event.nativeEvent.text);
+    }
+  }
+
+  render() {
+    const { label } = this.props;
+    return (
+      <ScrollView style={{flex: 1}}>
+        <View style={styles.container}>
+          <View style={styles.label}>
+            <Text>{label}</Text>
+          </View>
+          <TextInput
+            onEndEditing={this.onEndEditing}
+            defaultValue="1"
+            keyboardType="numeric"
+            style={styles.input}
+          />
+        </View>
+      </ScrollView>
+    );
+  }
+}
