92cbe3774ce9f9e027bb03b9b98af6b9339333ec
Brent Van Minnen <bjvanminnen@gmail.com>
2016-12-25 15:47:59 -0800

    destination point via LatLong

--- src/distanceUtils.js
+++ src/distanceUtils.js
@@ -1,4 +1,8 @@
 import haversine from 'haversine';
+import LatLong from './LatLong';
+
+import getStore from './redux/getStore';
+import { addPosition } from './redux/visitedPositions';
 
 export const distanceDiff = (one, two) => {
   return haversine(one, two, { unit: 'meter' });
@@ -9,3 +13,17 @@ export const isWithin = (one, two, meters) => {
 }
 
 // TODO - generate a location n meters away
+export const foo = (n, angle = 0) => {
+  const lat = 37.785834;
+  const long = -122.406417;
+
+  const latLong = new LatLong(lat, long);
+  const next = latLong.destinationPoint(n, angle);
+
+  const store = getStore();
+  store.dispatch(addPosition({
+    latitude: next.lat,
+    longitude: next.lon
+  }))
+};
+window.foo = foo;
